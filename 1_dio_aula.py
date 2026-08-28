10d85a2f1d663d18beff06956dcf0d30fe5e3ca4
Pedro <luispedro0810@gmail.com>
2026-08-28 12:11:37 -0300

    anotações

---
+++ 1_dio_aula.py
@@ -0,0 +1,18 @@
+arquivo = open('C:/Users/DVTI/Documents/ESTUDOS - PL/logica_com_python/dio/lorem.txt', 'r')
+
+#NOTE:  retorna todo o arquivo de uma vez no seu formato original, linha a linha
+#print(arquivo.read())
+
+#NOTE: retorna uma linha por vez
+#print(arquivo.readline())
+
+#NOTE: retorna todas as linhas no arquivo, em formato de uma lista de strings
+#print(arquivo.readlines())
+
+#NOTE: printa cada letra da primeira linha do arquivo
+#for linha in arquivo.readline():
+#    print(linha)
+
+#NOTE: printa cada linha do arquivo(agora printando todo o conjunto de palavras dentro do arquivo)
+#for linha in arquivo.readlines():
+#    print(linha)
---
+++ 3_dio_aula.py
@@ -0,0 +1,23 @@
+import os
+import shutil
+from pathlib import Path
+
+
+#NOTE: o Path faz com que o arquivo que você está executando se torne um objeto, podendo manipular os arquivos e etc, e o .parent faz com que o arquivo recebido pelo Path retorne um arquivo pra cima 
+ROOT_PATH = Path(__file__).parent
+
+#NOTE: cria uma nova pasta(com o os.mkdir) na pasta atual do arquivo
+#os.mkdir(ROOT_PATH / "meu-diretorio")
+
+#NOTE: criando o arquivo dentro da pasta que estamos 
+arquivo = open(ROOT_PATH / "novo.txt", 'w')
+arquivo.close()
+
+#NOTE: você coloca o caminho da pasta aonde o arquivo está, o nome do arquivo, e após isso o caminho novamente do arquivo e o seu nome modificado(isso tudo com o os.rename)
+#os.rename(ROOT_PATH / "novo.txt", ROOT_PATH / "modificando.txt")
+
+#NOTE: você coloca o caminho da pasta aonde o arquivo está, e o nome do arquivo que você quer excluir
+#os.remove(ROOT_PATH / "modificando.txt")
+
+#NOTE: você coloca o caminho da pasta aonde o arquivo está, depois a localização do diretorio para aonde o arquivo será movido e o nome do arquivo 
+shutil.move(ROOT_PATH / "novo.txt", ROOT_PATH / "meu-diretorio" / "novo.txt")
